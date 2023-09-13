aac38ce08b419d0061cc929395acea2bd62dd9a4
stirsi <144865498+stirsi@users.noreply.github.com>
2023-09-12 23:45:48 -0400

    Update SQL Movie-Rating Query Exercises.sql

--- SQL Movie-Rating Query Exercises.sql
+++ SQL Movie-Rating Query Exercises.sql
@@ -78,7 +78,7 @@ from (select title, max(stars) as mx, min(stars) as mn
 	group by title)
 order by rating_spread desc, title;
 
-/* Q8 Find the difference between the average rating of movies released before 1980 and 
+/* Q9 Find the difference between the average rating of movies released before 1980 and 
 the average rating of movies released after 1980. 
 (Make sure to calculate the average rating for each movie, then the average of those averages 
 for movies before 1980 and movies after. Don't just calculate the overall average rating before and after 1980.) */
